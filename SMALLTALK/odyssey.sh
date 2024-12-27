f9e9bac22a35f258135b1e3dc770f119ad26fac4
zjasina <Z.Jasina@o2.pl>
2024-12-27 01:27:19 +0100

    sun god stuff

--- SMALLTALK/odyssey.sh
+++ SMALLTALK/odyssey.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gst ./underworld_challenge.st ./characters.st ./world_map.st ./game_state.st ./main.st
+gst -g ./underworld_challenge.st ./characters.st ./world_map.st ./game_state.st ./main.st
